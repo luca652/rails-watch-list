55eeea4f580f0c1dc4f62ab5debae70bcaa109de
luca gastaldi <lucagastaldi82@gmail.com>
2023-05-11 19:35:20 +0100

    added root route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # get 'lists/new', to: 'lists#new', as: 'new_list'
   # post 'lists', to: 'lists#create'
   # get 'lists/:id', to: 'lists#show', as: 'list' do
-
+  get root to: "lists#index"
   resources :lists do
     resources :bookmarks, only: %i[new create]
     resources :movies
