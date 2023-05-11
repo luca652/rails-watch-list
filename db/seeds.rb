91612ca42f00726988f8a9c526bfb421c8e84cbb
luca gastaldi <lucagastaldi82@gmail.com>
2023-05-11 20:31:32 +0100

    fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,5 +18,5 @@ movies_serialized = URI.open(url).read
 movies = JSON.parse(movies_serialized)
 
 movies["results"].each do |movie|
-  Movie.create(title: movie['title'], overview: movie['overview'], poster_url: movie['poster_path'], rating: movie['vote_average'])
+  Movie.create(title: movie['title'], overview: movie['overview'], poster_url: "https://image.tmdb.org/t/p/original#{movie['poster_path']}", rating: movie['vote_average'])
 end
